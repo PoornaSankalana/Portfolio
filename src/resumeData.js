6f764ed6fa8cb963f55f3a5905155d7e10a2ce38
PoornaSankalana <sanka2016pps@gmail.com>
2022-12-08 10:31:23 +0530

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    imagebaseurl: "https://rbhatia46.github.io/",
+    name: "Poorna Sankalana",
+    role: "Full Stack Web and Mobile Application Developer",
+    linkedinId: "Poorna Sankalana",
+    facebookId: "Poorna Sankalana",
+    githubId: "PoornaSankalana",
+    email: "poorna.skl22@gmail.com",
+    tele: "070 243 1416",
+    roleDescription: "I like dabbling in various parts of frontend and backend development and like to learn about new technologies or simply play games in my free time.",
+    socialLinks: [{
+            name: "facebook",
+            url: "https://www.facebook.com/poorna.sankalana.3",
+            className: "fa fa-facebook",
+        },
+        {
+            name: "github",
+            url: "https://github.com/PoornaSankalana",
+            className: "fa fa-github",
+        },
+        {
+            name: "linkedin",
+            url: "https://www.linkedin.com/in/poorna-sankalana-67213620b/",
+            className: "fa fa-linkedin",
+        },
+    ],
+    aboutme: "I am currently a pre-final year student at Sri Lankan Institute of Information Technology and pursuing my BSc. from here. I am a self taught Full Stack Web Developer, currently diving deeper into Mobile Application Development. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    address: "Okkampitiya Road, Medagama, Buttala. Sri Lanka",
+    mobile: "070 243 1416",
+    education: [{
+            UniversityName: "British Council",
+            specialization: "Certificate Course in Spoken English",
+            MonthOfPassing: "Completed in 2019",
+            // YearOfPassing: "2020",
+            // Achievements: "Some Achievements",
+        },
+        {
+            UniversityName: "Sri Lankan Institute of Information Technology",
+            specialization: "Specialization in Software Engineering",
+            MonthOfPassing: "Still Reading From 2020",
+            // YearOfPassing: "2020",
+            // Achievements: "Some Achievements",
+        },
+        {
+            UniversityName: "University Of Moratuwa",
+            specialization: "Trainee Full Stack Developer",
+            MonthOfPassing: "Still Reading From 2020",
+            // YearOfPassing: "2020",
+            // Achievements: "Some Achievements",
+        },
+    ],
+    work: [{
+        CompanyName: "Antler IT Solutions",
+        specialization: "Intern - Software Engineer",
+        MonthOfLeaving: "Still Working From 2022 September",
+        // YearOfLeaving: "2018",
+        // Achievements: "Some Achievements",
+    }, ],
+    skillsDescription: "Your skills here",
+    skills: [{
+            skillname: "HTML5",
+        },
+        {
+            skillname: "CSS",
+        },
+        {
+            skillname: "React",
+        },
+        {
+            skillname: "Node",
+        },
+        {
+            skillname: "Photoshop",
+        },
+        {
+            skillname: "Illustrator",
+        },
+        {
+            skillname: "Javascript",
+        },
+        {
+            skillname: "Python",
+        },
+        {
+            skillname: "Java",
+        },
+        {
+            skillname: "SQL",
+        },
+        {
+            skillname: "MongoDB",
+        },
+        {
+            skillname: "C",
+        },
+    ],
+    portfolio: [{
+            name: "CareForYou Laboratories",
+            description: "Web Application",
+            imgurl: "images/portfolio/CareForU.png",
+            giturl: "https://github.com/PoornaSankalana/Online-Laboratory-Management-System-Frontend",
+        },
+        {
+            name: "BrainTech Institute",
+            description: "Web Application",
+            imgurl: "images/portfolio/BraintechInstitute.png",
+            giturl: "https://github.com/PoornaSankalana/ITP-frontEnd",
+        },
+        {
+            name: "FashionHub Online Fashion Store",
+            description: "Mobile Application",
+            imgurl: "images/portfolio/FashionHub.png",
+            giturl: "https://github.com/PoornaSankalana/ITP-frontEnd",
+        },
+    ],
+    testimonials: [{
+            description: "Truely Recommend this guy for your project development.",
+            name: "Trainee Software Engineer - Surge Global - Sasindu Nanayakkara",
+        },
+        {
+            description: "Truely Recommend this guy for your project development.",
+            name: "Assiciate Software Engineer - Arimac - Susith Rupasinghe",
+        },
+    ],
+};
+
+export default resumeData;
