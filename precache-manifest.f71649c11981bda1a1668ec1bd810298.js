42e3cfcbeda467917578988fabb70cfea205389e
PoornaSankalana <sanka2016pps@gmail.com>
2022-12-08 10:49:36 +0530

    Updates

---
+++ precache-manifest.f71649c11981bda1a1668ec1bd810298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdee363c249f641b9f0f",
+    "url": "/Portfolio/static/css/main.86e35239.chunk.css"
+  },
+  {
+    "revision": "bdee363c249f641b9f0f",
+    "url": "/Portfolio/static/js/main.bdee363c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "73b64ab3e5c9dce21dc5f5c705f0c1ab",
+    "url": "/Portfolio/index.html"
+  }
+];
